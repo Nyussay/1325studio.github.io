23501da2a205ead7b5da5de76fb159ab3d927ca2
Nyussay <nyuusss@icloud.com>
2019-12-11 10:52:18 +0300

    для портфолио

---
+++ js/services.js
@@ -0,0 +1,11 @@
+/*
+var price;
+var quantity;
+var total;
+
+price = 100;
+quantity = 120;
+total = price+quantity;
+
+var el = document.getElementById("cost");
+el.textContent = total + "руб."; */
